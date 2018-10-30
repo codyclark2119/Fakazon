148862b4ee151bccd088ef861a248a84fb4c85a9
cody clark <codyclark2119@gmail.com>
2018-10-29 19:18:27 -0500

    start

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Defining middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("client/public"));
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactfullstack");
+
+const userRoutes = require("./routes/user.js")
+app.use(userRoutes);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`Server now listening on PORT ${PORT}!`);
+});
