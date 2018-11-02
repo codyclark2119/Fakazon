2806f00311dce4382dbbb2ca9ccbb31b20daa3a5
Russell Ott <russellaott@gmail.com>
2018-11-01 22:18:46 -0500

    adding Header component and updates to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Login from "./componets/Login.js";
+import Header from "./componets/Header.js";
 import './App.css';
 
 class App extends Component {
