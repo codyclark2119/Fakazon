f6e478678359a516832a62fcb37f2416f4348aaf
Verhana <arrowfoxie@gmail.com>
2018-11-11 19:00:39 -0600

    navbar, jumbotron, fontawesome

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import "./Home.css";
 
 class Home extends Component {
  render() {
       return (
           
        <div>
-           Home.
+    
+                <div className="card card-image homeimg">
+            <div className="text-white text-center py-5 px-4 my-5">
+                <div>
+                <h2 className="card-title h1-responsive pt-3 mb-5 font-bold txtshd"><strong>The future of eCommerce is here</strong></h2>
+                <p className="mx-5 mb-5 txtshd">Never leave your house again! Shop from the comfort of your home.
+                </p>
+                </div>
+            </div>
+            </div>
+
        </div>
       );
     }
