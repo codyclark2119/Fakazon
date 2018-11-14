b292196632d797304c4ed75b0cfabb0cb95d79af
cody clark <codyclark2119@gmail.com>
2018-11-14 09:48:47 -0600

    save

--- models/index.js
+++ models/index.js
@@ -1,9 +1,6 @@
 module.exports = {
     User: require("./User"),
     Item: require("./Item"),
-<<<<<<< HEAD
-    Cart: require("./Cart")
-=======
+    Cart: require("./Cart"),
     UserSession: require("./UserSession")
->>>>>>> d469760049d398cbaa08fce0b6a51f0df9566574
 };
